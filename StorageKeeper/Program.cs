8c47dad67f34b67a11448a56ca07ff642f67c680
Galant96 <38892531+Galant96@users.noreply.github.com>
2023-01-17 19:14:41 +0000

    Modified Program - Commented main functionalities of the app

--- StorageKeeper/Program.cs
+++ StorageKeeper/Program.cs
@@ -6,6 +6,22 @@ namespace StorageKeeper
     {
         static void Main(string[] args)
         {
+            // 1. Say hello to user
+            // 2. Print menu
+                // 2.1 Create a storage catalogue
+                    // Provide name
+                        // Save
+                // 2.2 Show catalogue
+                // 2.3 Add item
+                    // Create id
+                    // Provide name
+                    // Provide quantity
+                // 2.4 Remove item
+                    // Provide name or id
+                        // Provide quantity
+                            // Remove
+                // 2.5 Exit App
+
             Console.WriteLine("Hello, World!");
         }
     }
